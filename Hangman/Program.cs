8eb3f0e5645006dac1fb486bec11b18220c23abd
Linas <66268230+linas92@users.noreply.github.com>
2021-03-18 19:40:17 +0200

    dsfgdfgdfg

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -22,12 +22,125 @@ namespace Hangman
             Console.WriteLine();
             Console.WriteLine("Press any key to close the console");
             Console.ReadKey();
+
+            Console.WriteLine(" --------|");
+            Console.WriteLine("|\n|\n|\n|\n|\n|\n|");
+            Console.WriteLine("-----");
         }
         public static void Hangman()
         {
-            List<char> correctGuesses = new List<char>();
-            List<char> incorrectGuesses = new List<char>();
+            Random RandomWordSelecter = new Random((int)DateTime.Now.Ticks);
+            readonly string[] = { ""}
+            int LivesLeft = 5;
+            bool Wienner = false;
+            int PlayersInput;
+            char Guess;
 
+            if ( == 1|| )
+            {
+
+            }
+
+            while (LivesLeft != 0 && Wienner != true)
+            {
+
+            }
         }
     }
 }
+
+/*ANIMALS:
+
+Elephant
+Lion
+Tiger
+Koala
+Shark
+Whale
+Octopus
+Snake
+Kangaroo
+Bear
+Lizard
+Squirrel
+Cow
+Crocodile
+Pelican
+Dolphin
+Bat
+Frog
+Badger
+Wolf
+Ox
+Reindeer
+Fox
+Sparrow
+Chimpanzee
+Pigeon
+Raven
+Ant
+Panda
+Walrus
+Spider
+Hedgehong
+Sheep
+Coyote
+Monkey
+Peacock
+Otter
+Turtle
+Hippopotamus
+Rabbit
+Giraffe
+Crow
+Ostrich
+Parrot
+Jellyfish
+Horse
+Mouse
+Chicken
+Deer
+Turkey
+Hamster
+Duck
+Shrimp
+Clams
+Goat
+Crab
+
+VARDAI:
+Tomas
+Darius
+Alfonsas
+Karolis
+Edgaras
+Margarita
+Simona
+Onute
+Inga
+Pamela
+
+LIETUVOS MIESTAI:
+Kaunas
+Vilnius
+Marijampole
+Vilkaviskis
+Alytus
+Elektrenai
+Jieznas
+Lazdijai
+Pakruojis
+Sakiai
+
+VALSTYBES:
+Australija
+Bulgarija
+Danija
+Filipinai
+Japonija
+Kanada
+Maldyvai
+Norvegija
+Prancūzija
+Senegalas
+*/
