fc9dbae6045944315eab9c5399e03c761f1f8dfe
Linas <66268230+linas92@users.noreply.github.com>
2021-03-18 18:55:22 +0200

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kartuves();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close the console");
+            Console.ReadKey();
+        }
+        public static void Kartuves()
+        {
+
+        }
+    }
+}
