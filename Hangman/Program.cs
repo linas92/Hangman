faa3d79bffc3df452f3eb10e3d3f941b64590aa4
Linas <66268230+linas92@users.noreply.github.com>
2021-03-22 18:11:41 +0200

    Worked ~15 hours on this and I still can't do this

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -18,28 +18,30 @@ namespace Hangman
             Console.WriteLine("3. World country names");
             Console.WriteLine("4. Animals");
             Console.WriteLine("Press 1,2,3 or 4 to choose the theme that you would like to play");
+
+            Console.WriteLine("\n\n\n\n\nUPDATE 101! THEMES ARE TOO MAINSTREAM AND YOU'RE ALREADY DEAD"); //"to hell with this, atleast its working" - mode engaged
         }
         public static void ThePrettyHangmansNoose()
         {
-            Console.WriteLine(" --------|");
-            Console.WriteLine("|\n|\n|\n|\n|\n|\n|");
+            Console.WriteLine(" ------|");
+            Console.WriteLine($"|      o\n|     \\|/     \n|      0\n|     / \\\n|     \n|     \n|");
             Console.WriteLine("-----");
-        } // LOOKS GREAT DOESN"T WORK
+        } // Looks good, does nothing
         public static void Theme()
         {
-            string []LithuanianNames = { "Tomas", "Darius", "Alfonsas", "Karolis", "Edgaras",
+            /*string []LithuanianNames = { "Tomas", "Darius", "Alfonsas", "Karolis", "Edgaras",
             "Margarita", "Simona", "Onute", "Olga", "Pamela"};
 
-            string []LithuanianCities = { "Kaunas", "Vilnius", "Marijampole",
+            //string []LithuanianCities = { "Kaunas", "Vilnius", "Marijampole",
                     "Vilkaviskis", "Alytus", "Jieznas", "Lazdijai", "Pakruojis", "Sakiai"};
 
-            string []WorldCountryNames = { "Australija", "Bulgarija", "Danija", "Filipinai",
+            //string []WorldCountryNames = { "Australija", "Bulgarija", "Danija", "Filipinai",
             "Japonija", "Kanada", "Maldyvai", "Norvegija", "Prancūzija", "Senegalas"};
 
-            string []Animals = { "Elephant", "Lion", "Tiger", "Koala", "Shark",
+           //string []Animals = { "Elephant", "Lion", "Tiger", "Koala", "Shark",
                     "Whale", "Octopus", "Snake", "Kangaroo", "Bear", "Lizard", "Squirrel","Cow",
                     "Crocodile", "Pelican", "Dolphin", "Bat", "Frog", "Badger","Wolf","Cow", "Ox",
-                    "Reindeer", "Fox", "Bat", "Sparrow", "Chimpanzee"};
+                    "Reindeer", "Fox", "Bat", "Sparrow", "Chimpanzee"};*/
         }
         static void Main(string[] args)
         {
@@ -49,8 +51,14 @@ namespace Hangman
 
             Random RandomWordSelecter = new Random((int)DateTime.Now.Ticks);
 
-            string LetterGuess = Theme()[RandomWordSelecter.Next(0, Theme().Length)]; //DOESN"T WORK
+            string[] ToHellWithThis = {"Tomas", "Darius", "Alfonsas", "Karolis", "Edgaras", "Margarita", "Simona", "Onute", "Olga",
+                "Pamela", "Kaunas", "Vilnius", "Marijampole",  "Vilkaviskis", "Alytus", "Jieznas", "Lazdijai", "Pakruojis", "Sakiai",
+                "Australija", "Bulgarija", "Danija", "Filipinai", "Japonija", "Kanada", "Maldyvai", "Norvegija", "Prancūzija",
+                "Senegalas",  "Elephant", "Lion", "Tiger", "Koala", "Shark", "Whale", "Octopus", "Snake", "Kangaroo", "Bear",
+                "Lizard", "Squirrel","Cow","Crocodile", "Pelican", "Dolphin", "Bat", "Frog", "Badger","Wolf","Cow", "Ox","Reindeer",
+                "Fox", "Bat", "Sparrow", "Chimpanzee"};
 
+            string LetterGuess = ToHellWithThis[RandomWordSelecter.Next(0, ToHellWithThis.Length)];
             string WordsCapsLocked = LetterGuess.ToUpper();
 
             StringBuilder LettersThatChangeWhenPlayerGuesses = new StringBuilder(LetterGuess.Length);
@@ -65,17 +73,17 @@ namespace Hangman
             string PlayersInput;
             int LettersThePlayerAlreadyGuessed = 0;
             char Guess;
-            int LivesLeft = 5;
-
+            int LivesLeft = 6;
+            /*
             string PlayerChoosingTheTheme = Console.ReadLine();
+                        
 
-            if (int.TryParse(PlayerChoosingTheTheme, out int Result) == false)
+            while (int.TryParse(PlayerChoosingTheTheme, out int Result) == false)
             {
                 Console.WriteLine("Wrong symbol, we only accept NUMBERS from 1 to 4");
                 PlayerChoosingTheTheme = Console.ReadLine();
             }
-
-            else if (PlayerChoosingTheTheme == "1" || PlayerChoosingTheTheme == "2" || PlayerChoosingTheTheme == "3")
+            if (PlayerChoosingTheTheme == "1" || PlayerChoosingTheTheme == "2" || PlayerChoosingTheTheme == "3")
             {
                 Console.WriteLine("Congrats on choosing the boring theme");
             }
@@ -83,10 +91,11 @@ namespace Hangman
             {
                 Console.WriteLine("You chose Animals. Congratulations my dear friend! You finally chose the fun one!");
             }
-
+            }
+            */
             while (!Wienner && LivesLeft > 0)
             {
-                Console.WriteLine("Human! Time to guess a letter");
+                Console.WriteLine("Time to guess a letter: ");
 
                 PlayersInput = Console.ReadLine().ToUpper();
                 Guess = PlayersInput[0];
@@ -94,23 +103,19 @@ namespace Hangman
                 if (GoodGuess.Contains(Guess))
                 {
                     Console.WriteLine($"You cannot try to guess this letter again as it was already guessed right {Guess}");
-                    ThePrettyHangmansNoose();
                     continue;
                 }
 
                 else if (BadGuess.Contains(Guess))
                 {
-                    Console.WriteLine("You've tried this and it didn't work!");
-                    Console.WriteLine($"You can do better than this guesss {Guess}");
-                    Console.WriteLine($"You have {LivesLeft} lives left");
-
-                    ThePrettyHangmansNoose();
+                    Console.WriteLine($"You've tried this and it didn't work. Try a different lettern than: { Guess}");
                     continue;
                 }
 
                 if (WordsCapsLocked.Contains(Guess))
                 {
                     GoodGuess.Add(Guess);
+
                     for (int i = 0; i < LetterGuess.Length; i++)
                     {
                         if (WordsCapsLocked[i] == Guess)
@@ -119,33 +124,27 @@ namespace Hangman
                             LettersThePlayerAlreadyGuessed++;
                         }
                     }
+
                     if (LettersThePlayerAlreadyGuessed == LetterGuess.Length)
                         Wienner = true;
                 }
                 else
                 {
                     BadGuess.Add(Guess);
-                    Console.WriteLine($"Bad guess N*gger! There was no \"{Guess}\" in that word!");
-                    Console.WriteLine("O\nM\nG\nDid you think I used the N word? Lol it was N A G G E R");
-                    Console.Clear();
-                    Console.WriteLine($"You have {LivesLeft--} lives left");
+                    Console.WriteLine($"Bad guess. Try again");
                     LivesLeft--;
                 }
+
                 Console.WriteLine(LettersThatChangeWhenPlayerGuesses.ToString());
-                if (Wienner)
-                {
-                    Console.WriteLine("YOU WON!");
-                    Console.WriteLine($"Email me \"yo_mamma@gmail.com\" your bank account number, ID card photo," +
-                        $"your first pets name, first school and you will get 1,000,000$ dollars!");
-                }
-                else
-                {
-                    Console.WriteLine("You lost  You're too cool so go back to school");
-                }
-                Console.WriteLine();
-                Console.WriteLine("Press any key to close the console");
-                Console.ReadKey();
             }
-        }
+
+                if (Wienner)
+                    Console.WriteLine("YOU WON! Email me all your personal info at yo_mamma@gmail.com to claim your prize");
+                else
+                    Console.WriteLine($"You lost. You're too cool so go back to school. \nThe word was: {LetterGuess}");
+
+                Console.WriteLine("\n\n\n\n\nPress any key to close the console");
+                Console.ReadKey();
+         }
     }
 }
